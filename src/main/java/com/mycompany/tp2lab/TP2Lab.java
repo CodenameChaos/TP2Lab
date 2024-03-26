d26e2526f83d8733c3da7857ebf6d01be6f0c118
CodenameChaos <oviedo.jonathan.e@gmail.com>
2024-03-25 22:12:12 -0300

    Interfaz grafica facheruli

--- src/main/java/com/mycompany/tp2lab/TP2Lab.java
+++ src/main/java/com/mycompany/tp2lab/TP2Lab.java
@@ -1,15 +1,34 @@
 package com.mycompany.tp2lab;
 
-import java.util.Scanner;
+import javax.swing.*;
 
 public class TP2Lab {
 
-    public static void main(String[] args) {
+       public static void main(String[] args) {
+        JFrame frame = new JFrame("Ingresar Nombre");
         
-        Scanner scanner = new Scanner(System.in);
+        // Esto es para configurar el tamaño de la interfaz
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        System.out.println("Ingrese su nombre.");
+        JPanel panel = new JPanel();
         
-        String nombre = scanner.nextLine();
+        JLabel label = new JLabel("Por favor, ingresa tu nombre:");
+        JTextField textField = new JTextField(20);
+        
+        JButton button = new JButton("Aceptar");
+        
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+        
+        frame.add(panel);
+        
+        frame.setVisible(true);
+        
+        button.addActionListener(e -> {
+            String nombre = textField.getText();
+            JOptionPane.showMessageDialog(null, "Hola, " + nombre + "! Bienvenid@ a esta interfaz grafica.");
+        });
     }
 }
